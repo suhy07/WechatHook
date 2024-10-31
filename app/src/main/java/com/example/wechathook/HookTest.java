dd3ab672c43588a98790e2470fcf51816c8074ad
suhy <suhy07@foxmail.com>
2024-10-31 16:43:25 +0800

    破解6.0版本登录

--- app/src/main/java/com/example/wechathook/HookTest.java
+++ app/src/main/java/com/example/wechathook/HookTest.java
@@ -1,59 +1,30 @@
 package com.example.wechathook;
 
 import android.util.Log;
-import android.widget.ListView;
+import android.widget.ListAdapter;
 
-import com.alibaba.fastjson.JSONObject; // 导入JSON库
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class HookTest implements IXposedHookLoadPackage {
+    final static String TAG = "HOOK_TEST";
 
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        Log.i("Demo: hookListView", loadPackageParam.packageName);
-        if (loadPackageParam.packageName.equals("com.tencent.mm")) {
-            XposedBridge.log("Demo: hookListView");
-            XposedBridge.log("com.tencent.mm");
-            Class clazz = loadPackageParam.classLoader.loadClass("com.tencent.mm.ui.conversation.ConversationListView");
-            // 确保使用正确的类名
-            XposedHelpers.findAndHookMethod(
-//                    "com.tencent.mm.ui.conversation.ConversationListView",
-//                    loadPackageParam.classLoader,
-//                    "setAdapter",
-//                    ListView.class,
-                    clazz,
-                    "smoothScrollToPositionFromTop",
-                    int.class, int.class,
-                    new XC_MethodHook() {
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    super.beforeHookedMethod(param);
-                    XposedBridge.log("找到辣! before");
-                    Log.i("HOOK", "找到辣 before");
-                }
+    @Override
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.tencent.mm")) {
+            return;
+        }
 
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    XposedBridge.log("找到辣! after");
-                    Log.i("HOOK", "找到辣 after");
-                }
-            });
-        } else if (loadPackageParam.packageName.equals("com.example.wechathook")) {
-            Log.i("HOOK", "main has Hooked!");
-            Class clazz = loadPackageParam.classLoader.loadClass("com.example.wechathook.MainActivity");
-            XposedHelpers.findAndHookMethod(clazz, "toastMessage", new XC_MethodHook() {
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    super.beforeHookedMethod(param);
-                    XposedBridge.log(" has Hooked!");
-                }
+        try {
+            // 获取d类的Class对象
+            Class<?> dClass = XposedHelpers.findClass("com.tencent.mm.protocal.d", lpparam.classLoader);
 
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    param.setResult("你已被劫持111");
-                    XposedBridge.log(" has Hooked!");
-                    Log.i("HOOK", "main has Hooked! after");
-                }
-            });
+            // 修改静态字段uGg的值
+            XposedHelpers.setStaticIntField(dClass, "uGg", 0x28003533);
+        } catch (Exception e) {
 
         }
     }
